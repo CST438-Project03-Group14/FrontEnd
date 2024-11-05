b3229c71d19dbe8f0af75bf8bf70265838fc68ac
ItzRikk <119906679+ItzRikk@users.noreply.github.com>
2024-11-05 10:45:07 -0800

    LogIn and SignUp setup

--- reactweb/src/App.js
+++ reactweb/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import LoginForm from './Components/LoginForm/LogIn';
+import SignupForm from './Components/SignupForm/SignUp';
+import Home from './Components/HomePage/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<LogIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/home" element={<Home />} />  {/* Add home route */}
+    </Routes>
   );
 }
 
